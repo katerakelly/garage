564608e832ba6a372596c3f5b49dfe82c8d27a4e
tsan <ahtsans@gmail.com>
2020-07-10 21:42:58 -0700

    Introduce SeedStream for TF tree

--- src/garage/tf/q_functions/discrete_mlp_q_function.py
+++ src/garage/tf/q_functions/discrete_mlp_q_function.py
@@ -1,6 +1,7 @@
 """Discrete MLP QFunction."""
 import tensorflow as tf
 
+from garage.experiment import deterministic
 from garage.tf.models import MLPDuelingModel, MLPModel
 from garage.tf.q_functions.q_function import QFunction
 
@@ -45,10 +46,12 @@ class DiscreteMLPQFunction(QFunction):
                  name=None,
                  hidden_sizes=(32, 32),
                  hidden_nonlinearity=tf.nn.relu,
-                 hidden_w_init=tf.initializers.glorot_uniform(),
+                 hidden_w_init=tf.initializers.glorot_uniform(
+                     seed=deterministic.get_tf_seed_stream()),
                  hidden_b_init=tf.zeros_initializer(),
                  output_nonlinearity=None,
-                 output_w_init=tf.initializers.glorot_uniform(),
+                 output_w_init=tf.initializers.glorot_uniform(
+                     seed=deterministic.get_tf_seed_stream()),
                  output_b_init=tf.zeros_initializer(),
                  dueling=False,
                  layer_normalization=False):
