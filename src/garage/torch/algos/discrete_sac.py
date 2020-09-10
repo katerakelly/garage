discrete-SAC: fix bug trying to backprop through torch.distribution
also remove double softmax of policy output

src/garage/torch/algos/discrete_sac.py
@@ -54,7 +54,7 @@ class DiscreteSAC(SAC):
         q1_pred = self._qf1(obs).gather(dim=-1, index=actions)
         q2_pred = self._qf2(obs).gather(dim=-1, index=actions)
 
-        pi = self.policy(next_obs)[0].probs # batch x action_dim of softmax values
+        pi = self.policy(next_obs)[0] # batch x action_dim of softmax values
         # don't take a log of 0!
         zero_locs = pi == 0.0
         zero_locs = zero_locs.float() * 1e-8
@@ -91,8 +91,7 @@ class DiscreteSAC(SAC):
 
         # just do this once here to avoid another forward call
         # when performing alpha update
-        pi_dist = self.policy(obs)[0]
-        pi = pi_dist.probs
+        pi = self.policy(obs)[0]
         # don't take a log of 0!
         zero_locs = pi == 0.0
         zero_locs = zero_locs.float() * 1e-8
@@ -111,6 +110,7 @@ class DiscreteSAC(SAC):
             alpha_loss.backward()
             self._alpha_optimizer.step()
 
+        pi_dist = torch.distributions.Categorical(probs=pi)
         policy_entropy = pi_dist.entropy()
 
         return policy_loss, qf1_loss, qf2_loss, policy_entropy

src/garage/torch/policies/categorical_mlp_policy.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from torch import nn
 
+from garage.torch import global_device
 from garage.torch.modules import MLPModule
 from garage.torch.policies.stochastic_policy import StochasticPolicy
 
@@ -57,7 +58,7 @@ class CategoricalMLPPolicy(StochasticPolicy):
             hidden_nonlinearity=hidden_nonlinearity,
             hidden_w_init=hidden_w_init,
             hidden_b_init=hidden_b_init,
-            output_nonlinearity=torch.nn.Softmax, # softmax output!
+            output_nonlinearity=None,
             output_w_init=output_w_init,
             output_b_init=output_b_init,
             layer_normalization=layer_normalization)
@@ -75,22 +76,41 @@ class CategoricalMLPPolicy(StochasticPolicy):
 
         """
         probs = self._module(observations)
-        dist = torch.distributions.Categorical(probs=probs)
-        return dist, {}
+        probs = torch.softmax(probs, dim=-1)
+        return probs, {}
 
     def _index_to_one_hot(self, arr):
+        # turn actions into 1-hot vectors of length action_dim
+        # to respect replay buffer interface
         one_hot = np.zeros((arr.size, self._action_dim))
         one_hot[np.arange(arr.size), arr] = 1
         return one_hot
 
     def get_action(self, observation):
-        action, info = super().get_action(observation)
-        # turn actions into 1-hot vectors of length action_dim
-        # to respect replay buffer interface
+        with torch.no_grad():
+            if not isinstance(observation, torch.Tensor):
+                observation = torch.as_tensor(observation).float().to(
+                    global_device())
+            observation = observation.unsqueeze(0)
+            probs, info = self.forward(observation)
+            dist = torch.distributions.Categorical(probs=probs)
+            action, info = dist.sample().squeeze(0).cpu().numpy(), {
+                k: v.squeeze(0).detach().cpu().numpy()
+                for (k, v) in info.items()
+            }
         one_hot_action = self._index_to_one_hot(action).squeeze(0)
         return one_hot_action, info
 
     def get_actions(self, observations):
-        actions, info = super().get_actions(observations)
+        with torch.no_grad():
+            if not isinstance(observations, torch.Tensor):
+                observations = torch.as_tensor(observations).float().to(
+                    global_device())
+            probs, info = self.forward(observations)
+            dist = torch.distributions.Categorical(probs=probs)
+            actions, info = dist.sample().cpu().numpy(), {
+                k: v.detach().cpu().numpy()
+                for (k, v) in info.items()
+            }
         one_hot_actions = self._index_to_one_hot(actions)
         return one_hot_actions, info