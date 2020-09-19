8f27e1ad121b74074f0984ceb7fddf9ebad146d2
Kate Rakelly <krakelly@berkeley.edu>
2020-09-18 17:17:38 -0700

    change is to == to check for length equality in sampling

--- src/garage/torch/algos/sac.py
+++ src/garage/torch/algos/sac.py
@@ -209,7 +209,7 @@ class SAC(RLAlgorithm):
                              next_observation=path['next_observations'],
                              terminal=path['dones'].reshape(-1, 1)))
                     path_returns.append(sum(path['rewards']))
-                assert len(path_returns) is len(runner.step_path)
+                assert len(path_returns) == len(runner.step_path)
                 self.episode_rewards.append(np.mean(path_returns))
                 for _ in range(self._gradient_steps):
                     policy_loss, qf1_loss, qf2_loss, policy_entropy = self.train_once()
